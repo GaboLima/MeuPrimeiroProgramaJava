752c4af4fef69e90659400d68b67696a8e57f010
katia <katia@192.168.15.62>
2021-08-11 20:09:08 -0300

    Alteração nos comentarios

--- meuSegundoProgramajava/src/meuSegundoProgramajava/Principal.java
+++ meuSegundoProgramajava/src/meuSegundoProgramajava/Principal.java
@@ -7,8 +7,10 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//saldação cria
 		System.out.println("Hello Friend \nwhats your name?");
 
+		//aaa
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		System.out.printf("Ola %s! \n", nome);
