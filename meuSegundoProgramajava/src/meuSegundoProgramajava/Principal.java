b9bd9d5a92843de6b6010ab0218feefa59475f2d
GaboLima <88804562+GaboLima@users.noreply.github.com>
2021-08-11 20:15:51 -0300

    Update Principal.java
    Alteração no comentário, adicionando mais um Print("Seja bem vindo!!!")

--- meuSegundoProgramajava/src/meuSegundoProgramajava/Principal.java
+++ meuSegundoProgramajava/src/meuSegundoProgramajava/Principal.java
@@ -7,13 +7,14 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//saldação cria
+		//saldaÃ§Ãµes
 		System.out.println("Hello Friend \nwhats your name?");
 
-		//aaa
+		//printando a variavel
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		System.out.printf("Ola %s! \n", nome);
+		System.out.printf("Seja bem vindo!!!");
 	}
 
 }
